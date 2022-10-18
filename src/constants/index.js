3de460aeca22c96f4e556409fd4a3c2a1e8ca1b9
justdoit-kyrie <ducndmse151198@gmail.com>
2022-10-19 06:26:47 +0700

    revert

--- src/constants/index.js
+++ src/constants/index.js
@@ -144,6 +144,14 @@ export const API_PATH = {
     getDetail: '/posts/:id',
     edit: '/posts/:id',
     getByCategory: '/posts/category',
+    savePost: '/repost',
+    getSave: '/repost/:id',
+    interactive: '/interactive',
+    like: '/interactive/like',
+    report: '/reports',
+  },
+  comment: {
+    post: '/comment/cmt',
   },
 };
 
